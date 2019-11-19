d44f92493162590f60d9d044a69037110aa58964
tjforeman <tylerforeman92@gmail.com>
2019-11-19 13:25:02 -0500

    its working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Register from './Auth/Register'
 import Login from './Auth/Login'
+import Landing from './Components/Landing'
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Link to='/register'>Register</Link>
-      <br></br>
-      <Link to='/login'>Login</Link>
       <div>
+      <Route exact path='/' component={Landing} />
       <Route path='/register' component={Register} />
       <Route path='/login' component={Login} />
       </div>
