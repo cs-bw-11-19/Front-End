c8992211ff533c2de16eed80fbad4460c1c5b5cf
tjforeman <tylerforeman92@gmail.com>
2019-11-19 21:10:06 -0500

    trying to move

--- client/src/Components/RoomInfo.js
+++ client/src/Components/RoomInfo.js
@@ -19,6 +19,25 @@ const RoomInfo = () => {
         })
     }, [])
 
+   const move = e => {
+        const token = localStorage.getItem('key')
+
+        const movement = {
+            direction : e
+        }
+
+        axios
+        .post(`${process.env.REACT_APP_API_URL}/api/adv/move`, movement, {
+            headers:{
+                Authorization: `Token ${token} `
+            }
+        .then(res =>{
+            console.log('move response', res)
+            setInfo(res.data)
+        })
+        })
+    }
+
     return(
         <div>
             <p>{info.title}</p>
@@ -26,6 +45,13 @@ const RoomInfo = () => {
             {/* {info.players.map(player =>{
                 return player
             })} */}
+        <div>
+            <h2>Directions</h2>
+            <button onClick={move('n')}>North</button>
+            <button onClick={move('s')}>South</button>
+            <button onClick={move('e')}>East</button>
+            <button onClick={move('w')}>West</button>
+        </div>
         </div>
     )
 }
