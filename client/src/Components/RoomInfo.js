1b01de6388692b63b9f76c562664b252bb8ee134
tjforeman <tylerforeman92@gmail.com>
2019-11-20 11:26:21 -0500

    basic movement working

--- client/src/Components/RoomInfo.js
+++ client/src/Components/RoomInfo.js
@@ -1,6 +1,9 @@
 import React,{useEffect,useState} from 'react'
 import axios from 'axios'
 import AxiosWithAuth from '../Utils/AxiosWithAuth'
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/roominfo.css'
 
 const RoomInfo = () => {
     const [info, setInfo] = useState([]);
@@ -15,6 +18,7 @@ const RoomInfo = () => {
         })
         .then(res => {
             console.log('res',res)
+            console.log('players',res.data.players)
             setInfo(res.data)
         })
     }, [])
@@ -35,22 +39,33 @@ const RoomInfo = () => {
         .then(res =>{
             console.log('move response', res)
             setInfo(res.data)
+            
         })
     }
-
+    if (!info.players){
+        return(<p>Loading...</p>)
+    }
+    else 
     return(
-        <div>
+        <div className ='room-container'>
+            <div className='display'>
+            <h4>Room</h4>
+            <br></br>
             <p>{info.title}</p>
+            <h4>Description</h4>
             <p>{info.description}</p>
-            {/* {info.players.map(player =>{
-                return player
-            })} */}
-        <div>
+            <h4>Other Players</h4>
+            <p>{info.players.map(player =>{
+                return <p>{player}</p>
+            })}
+            </p>
+            </div>
+        <div className='directions'>
             <h2>Directions</h2>
-            <button onClick={move('n')}>North</button>
-            <button onClick={move('s')}>South</button>
-            <button onClick={move('e')}>East</button>
-            <button onClick={move('w')}>West</button>
+            <Button className= 'dirbutton' onClick={e => move('n')}>North</Button>
+            <Button className= 'dirbutton' onClick={e => move('s')}>South</Button>
+            <Button className= 'dirbutton' onClick={e => move('e')}>East</Button>
+            <Button className= 'dirbutton' onClick={e =>  move('w')}>West</Button>
         </div>
         </div>
     )
